ec6cf1c074caf99249e824cb3fb907f432a5a683
miyabitanimchi <mmyybb.mm@gmail.com>
2022-12-21 01:23:21 -0800

    style: fix indent on services/index

--- services/index.js
+++ services/index.js
@@ -3,36 +3,36 @@ import { request, gql } from "graphql-request";
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
-    const query = gql`
-        query MyQuery {
-            postsConnection {
-                edges {
-                    node {
-                        author {
-                            bio
-                            name
-                            id
-                            photo {
-                                url
-                            }
-                        }
-                        createdAt
-                        slug
-                        title
-                        excerpt
-                        featuredImage {
-                            url
-                        }
-                        categories {
-                            name
-                            slug
-                        }
-                    }
-                }
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
             }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
         }
-    `
-    const result = await request(graphqlAPI, query);
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query);
 
-    return result.postsConnection.edges;
-}
+  return result.postsConnection.edges;
+};
