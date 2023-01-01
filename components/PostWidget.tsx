3b930b350b3232b808870bdd5f95a26c161fac3c
miyabitanimchi <mmyybb.mm@gmail.com>
2022-12-31 19:11:49 -0800

    feat: create query to get resent posts

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { format } from "date-fns";
+import Link from "next/link";
+import { getRecentPosts } from "../services";
 
-const PostWidget = () => {
+const PostWidget = ({ categories, slug }: any) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if (slug) {
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result));
+    }
+  }, []);
   return <div>PostWidget</div>;
 };
 
