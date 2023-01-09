6f3e57ab704456695a561b1977a7dc7df3d7bde2
miyabitanimchi <mmyybb.mm@gmail.com>
2023-01-08 23:52:25 -0800

    feat: add query to get comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
+import parse from "html-react-parser";
 
-const Comments = () => {
+const Comments = ({ slug }: any) => {
+  const [comments, setComments] = useState([]);
   return <h1>Comments</h1>;
 };
 
